8585dadffa912a4154d4f089239bc032c3029732
joel <joelbmolina@gmail.com>
2024-04-17 13:36:33 -0700

    add bubblesort and tests

---
+++ src/__tests__/bubble_sort.test.ts
@@ -0,0 +1,14 @@
+import { describe, expect, test } from "bun:test";
+import bubbleSort from "../bubble_sort";
+
+describe("bubble sort", () => {
+  test("sort in place with no return", () => {
+    expect(bubbleSort([])).toBeUndefined();
+  });
+  test("sort in ascending order", () => {
+    const data = [4, 2, 7, 1, 3];
+    bubbleSort(data);
+
+    expect(data).toBe([1, 2, 3, 4, 7]);
+  });
+});
