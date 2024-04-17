81e886ec41fe6b90d00c087579c1326031038712
joel <joelbmolina@gmail.com>
2024-04-17 15:41:59 -0700

    oopsies

---
+++ src/linked_list_s.ts
@@ -0,0 +1,10 @@
+export default class SLinkedList<T> {
+  public length: number;
+
+  constructor() {}
+
+  add(item: T): void {}
+  get(i: number): T | undefined {}
+  remove(i: number): T | undefined {}
+  peek(): T | undefined {}
+}
