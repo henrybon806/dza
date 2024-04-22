cbe6ee6038fa84e996791e542046ef602b9ed317
Henry Bonomolo <hbono806@icloud.com>
2024-04-21 21:55:29 -0700

    finished creating bubble sort ready for review

--- src/bubble_sort.ts
+++ src/bubble_sort.ts
@@ -1,1 +1,11 @@
-export default function bubbleSort(arr: number[]): void {}
+export default function bubbleSort(arr: number[]): void {
+    for(let i = 0; i < arr.length; i++){
+        for(let j = 0; j < arr.length - 1; j++){
+            if(arr[j] > arr[j+1]){
+                let placeholder = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j+1] = placeholder;
+            }
+        }
+    }
+}
